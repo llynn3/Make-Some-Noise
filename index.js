b8326eb239d3c6d29678391f526e0dff2fea9451
lynne <user@email.com>
2021-09-01 16:59:07 -0700

    got event listener and for each function to work, change tastedive url to get more info of the artists

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
 // const apiKey = "422965-project-P86D3GKY"
-// const tastediveURL = `https://tastedive.com/api/similar?q=${artistParam}&k=${apiKey}`
 
 const inputEl = document.querySelector("#blank");
-const containerEl = document.createElement('div');
 const button = document.querySelector("#search");
 
 
-const getArtistInfo = async () => {
+const getArtistResults = async () => {
     try {
         const artistName = inputEl.value;
-        const tastediveURL = `http://cors-lite.herokuapp.com/https://tastedive.com/api/similar?q=${artistName}&k=422965-project-P86D3GKY`
+        const tastediveURL = `http://cors-lite.herokuapp.com/https://tastedive.com/api/similar?info=1&q=${artistName}&k=422965-project-P86D3GKY`
         const response = await axios.get(tastediveURL);
-        const artists = response.data.Similar.Results;
+        const artists = response.data.Similar.Results
+        console.log(response);
+
+        const artistListEl = document.querySelector(".artist-list")
+
 
         artists.forEach((input) => {
-            const info = document.createElement("p");
-            info.innerText = input.Name;
 
-            containerEl.append(info);
+            console.log(input);
 
-            console.log(input.Name);
+            const artistsContainer = document.createElement("div");
+            artistListEl.append(artistsContainer);
+
+            const artistName = document.createElement("h3");
+            artistName.innerText = input.Name;
+            artistsContainer.appendChild(artistName);
+            const artistType = document.createElement("p");
+            artistType.innerText = input.Type;
+            artistsContainer.appendChild(artistType);
+            const artistWTeaser = document.createElement("p");
+            artistWTeaser.innerText = input.wTeaser;
+            artistsContainer.appendChild(artistWTeaser);
+            const artistYUrl = document.createElement("p");
+            artistYUrl.innerHTML = input.yUrl;
+            artistsContainer.appendChild(artistYUrl);
+
+            
 
     })
 
@@ -28,4 +44,5 @@ const getArtistInfo = async () => {
     }
 }
 
-button.addEventListener("click", getArtistInfo);
+
+button.addEventListener("click", getArtistResults);
