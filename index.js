29d301adf3ab74c44636116ed6d38e17fb743f80
lynne <user@email.com>
2021-09-02 00:33:28 -0700

    add dropdown of artists

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // const apiKey = "422965-project-P86D3GKY"
 
+const geniusToken = "NVCHblKgNHjrJQyuwmeXqHQuHelp9mXThm7spH2UhLF6Jt4vCFbs1lo74anCFOM3"
 const inputEl = document.querySelector("#blank");
 const button = document.querySelector("#search");
 
 
+const dropdown = document.querySelector('select');
+const dropdownContainer = document.querySelector("#info-container");
 
 const getArtistResults = async () => {
     try {
@@ -13,40 +16,53 @@ const getArtistResults = async () => {
         const artists = response.data.Similar.Results
 
         const artistListEl = document.querySelector(".artist-list")
-        const dropdown = document.querySelector("#dropdown");
-
-
 
         artists.forEach((input) => {
+        const option = document.createElement('option');
+        option.innerText = input.Name;
+        dropdown.appendChild(option);
 
-            const artistsContainer = document.createElement("div");
-            artistListEl.append(artistsContainer);
-
-            const artistName = document.createElement("h3");
-            // console.log(artistName);
-            artistName.innerText = input.Name;
-            artistsContainer.appendChild(artistName);
-            const artistWTeaser = document.createElement("p");
-            artistWTeaser.innerText = input.wTeaser;
-            artistsContainer.appendChild(artistWTeaser);
-            const artistYUrl = document.createElement("p");
-            artistYUrl.innerHTML = input.yUrl;
-            artistsContainer.appendChild(artistYUrl);
-
-            const artistsEl = document.createElement("option");
-            artistsEl.innerText = artistName
-            dropdown.append(artistName);
-            console.log(artistName);
-
-
-    })
+        });
 
     } catch (error) {
         console.log(error);
     }
 }
 
+const resultsAndInfo = async (event) => {
+    try {
+        const newArtist = event.target.value;
+
+        const url = `http://cors-lite.herokuapp.com/https://tastedive.com/api/similar?info=1&q=${newArtist}&k=422965-project-P86D3GKY`;
+        const response = await axios.get(url);
+        const dropdownArtists = response.data.Similar.Info;
+
+        dropdownArtists.forEach((artist) => {
+
+            const infoContainer = document.createElement("div");
+            dropdownContainer.append(infoContainer);
+
+            const artistName = document.createElement("h3");
+            artistName.innerText = artist.Name;
+            infoContainer.appendChild(artistName);
+            const artistPicture = document.createElement("div");
+            artistPicture.src = artistPicture;
+            infoContainer.appendChild(artistPicture);
+            const artistWTeaser = document.createElement("p");
+            artistWTeaser.innerText = artist.wTeaser;
+            infoContainer.appendChild(artistWTeaser);
+            const artistYUrl = document.createElement("p");
+            artistYUrl.innerHTML = artist.yUrl;
+            infoContainer.appendChild(artistYUrl);
+
+        });
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 
 
 button.addEventListener("click", getArtistResults);
-dropdown.addEventListener("change", getArtistResults);
+dropdown.addEventListener("change", resultsAndInfo);
