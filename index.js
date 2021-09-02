3ba5c50db1a13edbdd2b93a7b10e4089b794f002
lynne <user@email.com>
2021-09-01 19:54:26 -0700

    trying to add dropdown

--- index.js
+++ index.js
@@ -4,30 +4,28 @@ const inputEl = document.querySelector("#blank");
 const button = document.querySelector("#search");
 
 
+
 const getArtistResults = async () => {
     try {
         const artistName = inputEl.value;
         const tastediveURL = `http://cors-lite.herokuapp.com/https://tastedive.com/api/similar?info=1&q=${artistName}&k=422965-project-P86D3GKY`
         const response = await axios.get(tastediveURL);
         const artists = response.data.Similar.Results
-        console.log(response);
 
         const artistListEl = document.querySelector(".artist-list")
+        const dropdown = document.querySelector("#dropdown");
+
 
 
         artists.forEach((input) => {
-
-            console.log(input);
 
             const artistsContainer = document.createElement("div");
             artistListEl.append(artistsContainer);
 
             const artistName = document.createElement("h3");
+            // console.log(artistName);
             artistName.innerText = input.Name;
             artistsContainer.appendChild(artistName);
-            const artistType = document.createElement("p");
-            artistType.innerText = input.Type;
-            artistsContainer.appendChild(artistType);
             const artistWTeaser = document.createElement("p");
             artistWTeaser.innerText = input.wTeaser;
             artistsContainer.appendChild(artistWTeaser);
@@ -35,7 +33,11 @@ const getArtistResults = async () => {
             artistYUrl.innerHTML = input.yUrl;
             artistsContainer.appendChild(artistYUrl);
 
-            
+            const artistsEl = document.createElement("option");
+            artistsEl.innerText = artistName
+            dropdown.append(artistName);
+            console.log(artistName);
+
 
     })
 
@@ -45,4 +47,6 @@ const getArtistResults = async () => {
 }
 
 
+
 button.addEventListener("click", getArtistResults);
+dropdown.addEventListener("change", getArtistResults);
